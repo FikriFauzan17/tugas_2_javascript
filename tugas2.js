14ccb2a1dd16a9541ff7e886dffbb03ab391072f
User-Niomic-Fikri <fikrifauzan17@gmail.com>
2020-04-15 13:08:22 +0700

    Tugas 2 JavaScript
    tugas ke 2 javascript

---
+++ tugas2.js
@@ -0,0 +1,19 @@
+var number = 2020;
+var string = "qwertyuiopasdfghjklzxcvbnm";
+var boolean = 50;
+if(boolean<70)
+boolean=true;
+else
+boolean=false;
+
+var kosong = null;
+var undifined;
+var array = [1,2,3,4,5,6,7,8,9,0]
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+console.log(kosong);
+console.log(undifined);
+console.log(array);
+console.log(array[6]);
